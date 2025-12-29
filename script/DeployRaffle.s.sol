4bd744a5bf6e1f667447dd507f3696d569faa3f8
GitAniket98 <aniketchaudhary9838@gmail.com>
2025-12-29 12:39:50 +0000

    Added Deploy script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,9 +3,27 @@ pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
     function run() external {}
 
-    function deployContract() public returns (Raffle) {}
+    function deployContract() public returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig
+            .getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            networkConfig.entranceFee,
+            networkConfig.interval,
+            networkConfig.vrfCoordinator,
+            networkConfig.gasLane,
+            networkConfig.subscriptionId,
+            networkConfig.callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return raffle;
+    }
 }
